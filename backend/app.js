e14bef516411b209283b284e46cd00fa51274e22
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-01-27 17:47:56 +0530

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,26 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
 
+dotenv.config({ path: './config.env' });
+
+require('./db/conn');
+
+app.use(express.json());
+
+app.use(require('./router/auth'));
+app.use(require('./router/productRoutes'));
+
+const PORT = process.env.PORT;
+
+
+app.listen(PORT,()=> {
+    console.log(`server is running at port no ${PORT}`);
+})

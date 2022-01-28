d40349974c813cf3a7a2cf020218da3277650595
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-01-28 10:42:17 +0530

    Add files via upload

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
+const DB = process.env.DATABASE;
 
+mongoose.connect(DB,{
+    useNewUrlParser:true
+    // useCreateIndex:true
+}).then(() => {
+    console.log(`connection successful`);
+}) .catch((err) => console.log(err));
